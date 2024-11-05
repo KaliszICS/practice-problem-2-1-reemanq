9572629298eebe5abddef2b42070e8404e6723b5
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 22:41:21 -0500

    Update PP2_1.py

--- PP2_1.py
+++ PP2_1.py
@@ -1,10 +1,20 @@
 
 
 def q1(): 
+  num = int(input("Input an integer: "))
+  if (num % 2) == 0:
+    print(f"{num} is even")
+  if (num % 2) == 1:
+    print(f"{num} is odd")
   #Write Assignment code here
 
 
 def q2(): 
+  name = input("Input a name: ")
+  if name == "Kalisz":
+    print("teacher")
+  else:
+    print("student")
   #Write Assignment code here
 
 
@@ -12,5 +22,5 @@ def q2():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
